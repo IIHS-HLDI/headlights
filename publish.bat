fb6729b3dfc19b998a5d05400ebfbaae30334880
Drew Brethwaite <abrethwaite@iihs.org>
2016-08-12 08:23:26 -0400

    Adjusted publish.bat

---
+++ publish.bat
@@ -0,0 +1,2 @@
+robocopy . "\\IIHS\IIHSDrive\VRC\Private\DIAdem\diadem" /MIR /Z /XD ".git" ".vs" ".vscode" /XF "diadem.sln" "publish.bat" "vwd.webinfo" "web.config" "web.debug.config"
+IF %ERRORLEVEL% LEQ 2 exit 0
