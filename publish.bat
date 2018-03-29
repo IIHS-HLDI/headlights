6ecb5ee74d3a7c70e0bf3100f76b3478bd4eb1e0
Judy Dixon <jdixon@iihs.org>
2018-03-29 16:07:03 -0400

    Fix to indicate error when robocopy really fails.

--- publish.bat
+++ publish.bat
@@ -1,2 +1,4 @@
 robocopy . "\\IIHS\IIHSDrive\VRC\Private\DIAdem\headlights" /MIR /Z /XD ".git" ".vs" ".vscode" /XF "diadem.sln" "publish.bat" "vwd.webinfo" "web.config" "web.debug.config"
-IF %ERRORLEVEL% LEQ 2 exit 0
+IF %ERRORLEVEL% LEQ 3 exit /b 0
+echo %ERRORLEVEL%
+exit /b %ERRORLEVEL%
