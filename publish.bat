5d54c4e289e27a331f722a73e900ea824ee88f12
Mike Powell <mpowell@iihs.org>
2023-04-04 09:22:39 -0400

    Remove publish.bat file

--- publish.bat
+++
@@ -1,4 +0,0 @@
-robocopy . "\\IIHS\IIHSDrive\VRC\Private\DIAdem\headlights" /MIR /Z /XD ".git" ".vs" ".vscode" /XF "diadem.sln" "publish.bat" "vwd.webinfo" "web.config" "web.debug.config"
-IF %ERRORLEVEL% LEQ 3 exit /b 0
-echo %ERRORLEVEL%
-exit /b %ERRORLEVEL%
